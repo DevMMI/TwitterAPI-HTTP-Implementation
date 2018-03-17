db3b7f7213bd26c30e5fa8cf6b5eea4468ff8d9f
Mohamed Mohamud <Moham893@umn.edu>
2018-03-16 20:37:54 -0500

    updated server

--- server.py
+++ server.py
@@ -5,180 +5,103 @@
 import os
 import stat
 import sys
-#import http.client
+import re
 import requests
-import logging
-import http.client
-import hmac
-import hashlib
-import urllib
-import urllib.parse
-import time
-import socket
-import random
-import secrets
 import string
-import base64
-import json
 import twitter
 from requests_oauthlib import OAuth1
 
-def _generate_signature(data, key):
-    """ Create the signed message from api_key and string_to_sign """
-    #return hmac.new(key, data, hashlib.sha1).hexdigest()
-    return base64.b64encode(hmac.new(key, data, digestmod=hashlib.sha1).digest())
-
-def query(queryurl, client_key, client_secret, resource_owner_key, resource_owner_secret):
-        headeroauth = OAuth1(client_key, client_secret, resource_owner_key,
-        resource_owner_secret, signature_type = 'auth_header')
-
-        return requests.get(queryurl, auth = headeroauth)
-
-def _generate_nonce():
-    secret = secrets.token_hex(16)
-    return str(secret)
-
-def postImage(src, consumerKey, cons_secret, myToken, tok_secret): # returns media id
-    api = twitter.Api(consumer_key = consumerKey, consumer_secret = cons_secret, access_token_key = myToken, access_token_secret=tok_secret)
+def postImage(src, auth): # returns media id
+    ''' Posts a image to twitter's server, returns an image ID '''
+    print("post image tweet")
+    access_token = auth[0]
+    access_token_secret = auth[1]
+    consumer_key = auth[2]
+    consumer_secret = auth[3]
+    api = twitter.Api(consumer_key = consumer_key, consumer_secret = consumer_secret, access_token_key = access_token, access_token_secret=access_token_secret)
     return api.UploadMediaChunked(media=src)
 
-def sendTweet():
-    debug = False
-    #debug = True
+def postTweetWithImage(image, status, auth, debug=False):
+    ''' Posts a tweet using the requests library,
+     along with an image uploaded with a prebuilt twitter library '''
+
     base_url = "https://api.twitter.com/1.1/statuses/update.json?"
     if debug:
         base_url = "localhost:9001"
 
-    # included entities
-    entities = "true"
-    encoded_incl_entities_key = urllib.parse.quote("include_entities", safe='')
-    encoded_incl_entities = urllib.parse.quote(entities, safe='')
-
-    # oauth_consumer_key
-    consumerKey = "pGPSO52SDNFh9mw9bjs3UlyTt"
-    encoded_cons_key_key = urllib.parse.quote("oauth_consumer_key", safe='')
-    encoded_cons_key = urllib.parse.quote(consumerKey, safe='')
-
-    # oath_nonce
-    nonce = _generate_nonce()
-    encoded_nonce_key = urllib.parse.quote("oauth_nonce", safe='')
-    encoded_nonce = urllib.parse.quote(nonce, safe='')
-
-    # oauth_signature_method
-    signatureMethod = "HMAC-SHA1"
-    encoded_sign_meth_key = urllib.parse.quote("oauth_signature_method", safe='')
-    encoded_sign_meth = urllib.parse.quote(signatureMethod, safe='')
-
-    # oauth_timestamp
-    timestamp = time.time()
-    timestamp_str = str(int(timestamp))
-    print("time : "+timestamp_str)
-    encoded_timestamp_key = urllib.parse.quote("oauth_timestamp", safe='')
-    encoded_timestamp= urllib.parse.quote(timestamp_str, safe='')
-
-    # oauth_token
-    myToken = "718628746278879232-rPx8JLxwQuvllwz5JT6mnBZRTTOKLf9"
-    encoded_token_key = urllib.parse.quote("oauth_token", safe='')
-    encoded_token = urllib.parse.quote(myToken, safe='')
-
-    # oauth_version
-    version = "1.0"
-    encoded_version_key = urllib.parse.quote("oauth_version", safe='')
-    encoded_version = urllib.parse.quote(version, safe='')
-
-    # status
-    status = "hello world"
-    encoded_status_key = urllib.parse.quote("status", safe='')
-    encoded_status = urllib.parse.quote(status, safe='')
-
-    # Build 'DST' Header String
-    DST = ""
-    keys = [encoded_incl_entities_key, encoded_cons_key_key, encoded_nonce_key, encoded_sign_meth_key, encoded_timestamp_key, encoded_token_key, encoded_version_key, encoded_status_key]
-
-    elements = [encoded_incl_entities, encoded_cons_key, encoded_nonce,encoded_sign_meth, encoded_timestamp, encoded_token, encoded_version, encoded_status]
-
-    # Build header
-    for i in range(0, len(elements)):
-        if i == (len(elements) - 1):
-            DST = DST + keys[i] + "=" + elements[i]
-        else:
-            DST = DST + keys[i] + "=" + elements[i] + "&"
-
-    # Build full signature base string
-    header = "POST"
-    encoded_url = urllib.parse.quote(base_url, safe='')
-    encoded_parameter_string = urllib.parse.quote(DST, safe='')
-    data = header + "&" + encoded_url + "&" + encoded_parameter_string
-
-
-    # percent encode consumer secret
-    cons_secret = "8trRaoFQN6uanT514CWHyEf161We5b1wY66fTYeFYXPiHU9Cim"
-    encoded_cons_secret = urllib.parse.quote(cons_secret, safe='')
-
-    # percent encode token secret
-    tok_secret = "Tf8DKGNKk4QmDuiVFPXgHFUJilFOGHsCH0U6ahBFonTD2"
-    encoded_tok_secret = urllib.parse.quote(tok_secret, safe='')
-
-    # signing key
-    sign_key = encoded_cons_secret + "&" + encoded_tok_secret
-    #print("data : "+ data + "\n")
-    print("key : "+ str(sign_key) + "\n")
-    # generate signature
-    #key = bytearray(sign_key.encode("utf8").strip())
-    #data = bytearray(data.encode("utf8").strip())
-    key = bytes(sign_key.encode("utf8"))
-    data = bytes(data.encode("utf8"))
-    #encoded_signature = _generate_signature(data, key)
-    encoded_signature = str(OAuth1(consumerKey, cons_secret, myToken, tok_secret))
-    encoded_signature = urllib.parse.quote(encoded_signature, safe='')
-    encoded_signature_key = urllib.parse.quote("oauth_signature", safe='')
-
-
-    # encoded_signature_base_two = urllib.parse.quote(signature, safe='')
-    # encoded_signature_base_64 = str(base64.b64encode(encoded_signature_base_two.encode()))
-    # encoded_signature_one = encoded_signature_base_64[2:]
-    #encoded_signature = urllib.parse.quote(signature, safe='')
-    #print("output : "+ signature.decode("utf-8") )
-    #print("signature : " + encoded_signature)
-
-    # make auth header
-    headerKeys = [encoded_cons_key_key, encoded_nonce_key, encoded_signature_key, encoded_sign_meth_key, encoded_timestamp_key, encoded_token_key,encoded_version_key]
-    headerValues = [encoded_cons_key, encoded_nonce, encoded_signature, encoded_sign_meth, encoded_timestamp, encoded_token, encoded_version]
-
-    auth_head = "OAuth "
-    # Build header
-    for i in range(0, len(headerKeys)):
-        if i == (len(headerKeys) - 1):
-            auth_head = auth_head + headerKeys[i] + "=" + "\"" + headerValues[i] + "\" "
-        else:
-            auth_head = auth_head + headerKeys[i] + "=" + "\"" + headerValues[i] + "\"" + ", "
-
-    print("Header : " +auth_head)
-    # begin connection
-    conn = http.client.HTTPSConnection("api.twitter.com")
-    if debug:
-        conn = http.client.HTTPConnection("localhost:9001")
+    # extract Credentials
+    access_token = auth[0]
+    access_token_secret = auth[1]
+    consumer_key = auth[2]
+    consumer_secret = auth[3]
 
     # upload media
-    image_id = postImage("hello.png", consumerKey, cons_secret, myToken, tok_secret)
-
+    image_id = postImage(image, auth)
 
     headers = {"Accept": "*/*", "Connection": "close", "User-Agent": "OAuth gem v0.4.4",\
-     "Content-Type": "application/x-www-form-urlencoded","Authorization": auth_head, "status": status, "media_ids": image_id, "attachment_url": "https://twitter.com/papi_ahmedd/status/974087807739351042"}
-
+     "Content-Type": "application/x-www-form-urlencoded", "status": status, "media_ids": image_id}
 
     if debug:
-         #conn = http.client.HTTPConnection("localhost:9001")
-         #conn.request("POST", "/", "status="+encoded_status, headers)
          r = requests.post("http://localhost:9001", data=headers)
          print(r.status_code, r.reason)
     else:
-        rauth = OAuth1(consumerKey, cons_secret, myToken, tok_secret)
+        rauth = OAuth1(consumer_key, consumer_secret, access_token, access_token_secret)
         r = requests.post("https://api.twitter.com/1.1/statuses/update.json?", data=headers, auth = rauth)
         print(r.status_code, r.reason)
-        #conn.request("POST", "/1.1/statuses/update.json?", "status="+encoded_status, headers)
-        #resp, content = client.request("'https://api.twitter.com/1.1/statuses/update.json?", method="POST", body=status, headers=headers )
+
+def postTweet(status, auth, debug=False):
+    ''' Posts a tweet using the requests library '''
+
+    print("post regular tweet")
+    sys.stdout.flush()
+
+    # extract Credentials
+    access_token = auth[0]
+    access_token_secret = auth[1]
+    consumer_key = auth[2]
+    consumer_secret = auth[3]
+    base_url = "https://api.twitter.com/1.1/statuses/update.json?"
+
+    if debug:
+        base_url = "localhost:9001"
+
+    headers = {"Accept": "*/*", "Connection": "close", "User-Agent": "OAuth gem v0.4.4",\
+     "Content-Type": "application/x-www-form-urlencoded", "status": status}
+
+    if debug:
+         r = requests.post("http://localhost:9001", data=headers)
+         print(r.status_code, r.reason)
+    else:
+        rauth = OAuth1(consumer_key, consumer_secret, access_token, access_token_secret)
+        r = requests.post("https://api.twitter.com/1.1/statuses/update.json?", data=headers, auth = rauth)
+        print(r.status_code, r.reason)
 
 
 if __name__ == '__main__':
-  sendTweet()
+    print("Type your tweet, or an image file followed by your tweet")
+    tweet = input("")
+
+    ## Setup Authentification ##
+
+    # Personal Credentials
+    access_token = #######################################
+    access_token_secret = #######################################
+
+    # Application Credentials
+    consumer_key = #######################################
+    consumer_secret = #######################################
+
+    auth = [access_token, access_token_secret, consumer_key, consumer_secret]
+
+    ## Setup Tweet ##
+    # Check if tweet has an image file
+    words = tweet.split(" ")
+    first_word = words[0].lower()
+    pattern = '.*(gif|jpg|jpeg|tiff|png)'
+
+    if re.match(pattern, first_word, flags=0):
+        # send a tweet with an image
+        postTweetWithImage(first_word, words[1:], auth)
+    else:
+        # send a regular tweet
+        postTweet(words[1:], auth)
